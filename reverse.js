0a22638981d1921eece2505b60d5793c39495062
MattLawson98 <matthewlawson50@gmail.com>
2022-09-26 16:52:20 -0700

    Added comments

--- reverse.js
+++ reverse.js
@@ -1,8 +1,9 @@
-
-
+//This kata will take a input via node and reverse it
+//removed takes away the first to arguments that node provides these are not the inputs we want
 let removed = process.argv.splice(0,2);
 let arr = process.argv
 console.log(arr);
+//cycle through the arr of inputs given and reverse each one.
 for (let w = 0; w < arr.length; w++) {
   let newString = "";
   for (let i = arr[w].length - 1; i >= 0; i--) {
